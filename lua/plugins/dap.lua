309713cc6811e6d12b7083e2935f68197542905c
Tomas Protivinsky <tomas.protivinsky@gmail.com>
2024-02-03 22:36:33 +0100

    cleanup

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -112,29 +112,6 @@ return {
   },
 
   config = function()
-    -- local dap = require 'dap'
-    -- local dapui = require 'dapui'
-    --
-    -- require("mason").setup()
-    -- require('mason-nvim-dap').setup {
-    --   automatic_installation = true,
-    --   handlers = {},
-    --   ensure_installed = { "debugpy" },
-    -- }
-    --
-    -- -- Basic debugging keymaps, feel free to change to your liking!
-    -- vim.keymap.set('n', keys.debug.start.key, dap.continue, { desc = keys.debug.start.desc })
-    -- vim.keymap.set('n', keys.debug.step_into.key, dap.step_into, { desc = keys.debug.step_into.desc })
-    -- vim.keymap.set('n', keys.debug.step_over.key, dap.step_over, { desc = keys.debug.step_over.desc })
-    -- vim.keymap.set('n', keys.debug.step_out.key, dap.step_out, { desc = keys.debug.step_out.desc })
-    -- vim.keymap.set('n', keys.debug.toggle_breakpoint.key, dap.toggle_breakpoint, { desc = keys.debug.toggle_breakpoint.desc })
-    -- vim.keymap.set('n', keys.debug.set_breakpoint.key, function()
-    --   dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-    -- end, { desc = keys.debug.set_breakpoint.desc })
-    --
-    -- -- dapui.setup {}
-    -- -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    -- -- vim.keymap.set('n', keys.debug.toggle_ui.key, dapui.toggle, { desc = keys.debug.toggle_ui.desc })
 
     vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
 
@@ -146,9 +123,5 @@ return {
       )
     end
 
-    -- -- Install golang specific config
-    -- -- require('dap-go').setup()
-    -- local path = require("mason-registry").get_package("debugpy"):get_install_path()
-    -- require("dap-python").setup(path .. "/venv/bin/python")
   end,
 }
